a4a2cd809589760fa0f4c3901f34786ee93d8ac8
Eddsalasgit <edslgn.1234@gmail.com>
2022-03-29 22:06:33 -0600

    github link

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,6 +10,9 @@ export default function HomeScreen({ onPress }){
     return (
       <View style={styles.container}>
         <Text>Home Screen</Text>
+        <Text style={{color:'blue'}} onPress={()=> NativeLinkingManager.openURL("https://github.com/Eddsalasgit/libreria1")}>
+            GitHub
+          </Text>
         <ButtonComponent title="Logout" onPress={onPress} color="#55DA12" />
       </View>
     )
